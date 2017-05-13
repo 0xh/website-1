fcf3da9d426b6d5a10c9b304495737b2194fe0ab
MichaelD <michael.e.deck@outlook.com>
2017-05-13 16:44:10 -0400

    Update webpack script

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,7 @@ var path = require('path');
 mix.less('resources/assets/less/app.less', 'public/css')
         .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js/sweetalert.min.js')
         .copy('node_modules/sweetalert/dist/sweetalert.css', 'public/css/sweetalert.css')
+        .copyDirectory('resources/assets/img', 'public/img')
         .js('resources/assets/js/app.js', 'public/js')
         .webpackConfig({
             resolve: {
